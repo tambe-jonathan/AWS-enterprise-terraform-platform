bb770ec75ba7054dd09f7474e9d5c7a1afa05fd1
agbor jonathan tambe <155083485+tambe-jonathan@users.noreply.github.com>
2026-04-16 22:39:10 +0300

    feat(lb): add application load balancer and routing configuration

---
+++ modules/load_balancer/main.tf
@@ -0,0 +1,25 @@
+resource "aws_lb" "main_alb" {
+  name               = "main-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnets
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
