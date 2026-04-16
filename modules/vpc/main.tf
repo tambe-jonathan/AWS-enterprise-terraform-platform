78e96113455955d2de050d7785e431cd5679f930
agbor jonathan tambe <155083485+tambe-jonathan@users.noreply.github.com>
2026-04-16 22:34:05 +0300

    feat(network): add VPC and subnet configuration

---
+++ modules/vpc/main.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = { Name = "production-vpc" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+  tags = { Name = "public-subnet-${count.index}" }
+}
+
+# Private Subnets (App Tier)
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+  availability_zone = var.azs[count.index]
+  tags = { Name = "private-app-subnet-${count.index}" }
+}
